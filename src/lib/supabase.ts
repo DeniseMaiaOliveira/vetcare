5f18f6a6b204fe135f017806f8c7caa053644665
DeniseMaiaOliveira <deniseon06@hotmail.com>
2026-05-17 13:22:51 -0300

    Instalar supabase

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://bmlowyvpsiazzrarwkuk.supabase.co'
-const supabaseKey = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJtbG93eXZwc2lhenpyYXJ3a3VrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg5NjUwNjQsImV4cCI6MjA5NDU0MTA2NH0.QoDrFCgU_z_SHFfbSEwN3ivvKGCTc1NBmygVDzWLytI
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
